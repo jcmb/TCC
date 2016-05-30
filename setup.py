cfc73006b061ed3b9e6e0994730826f08b683beb
jcmb <Geoffrey@JCMBSoft.com>
2016-05-30 10:04:07 -0600

    Add TCC_Check_User.py to setup, add TSD_Process as a module

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(name='TCC Utils',
       author_email='Geoffrey@jcmbsoft.com',
       url='https://jcmbsoft.com/',
       license="MIT For Trimble, GPL V3 for everyone else",
-      py_modules=['TCC'],
-      scripts=['TCC_Files/TCC_Files.sh','TCC_Files/TCC_FileAccess.py','TSD_Tools/TSD_Check.py','TSD_Tools/TSD_Download.py']
+      py_modules=['TCC','TSD_Process'],
+      scripts=['TCC_Check_User.py','TCC_Files/TCC_Files.sh','TCC_Files/TCC_FileAccess.py','TSD_Tools/TSD_Check.py','TSD_Tools/TSD_Download.py']
      )
