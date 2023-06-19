71196012ee617d76b1aab8c62c0e7fd2fbe6f5ce
jcmb <jcmb@users.noreply.github.com>
2023-06-19 16:33:23 +0300

    Add timeouts and PL Support
    Also added a Generator instead of returning a list for the PL ID's

--- TCC.py
+++ TCC.py
@@ -1,11 +1,11 @@
 
 import logging
 import requests
-import urllib
+import urllib.parse
 from pprint import pprint
 from datetime import datetime
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 
@@ -38,7 +38,7 @@ class TCC:
         self.Logged_In=False
 
         try:
-            r=requests.get(self.TCC_API + "login?username={}&orgname={}&password={}&applicationkey={}".format(self.USER,self.ORG,self.PASSWD,Application_Type))
+            r=requests.get(self.TCC_API + "login?username={}&orgname={}&password={}&applicationkey={}".format(self.USER,self.ORG,self.PASSWD,Application_Type),timeout=300)
 #            pprint(r.json())
             if r.status_code != 200:
                 self.logger.error("Could not login TCC")
@@ -55,7 +55,7 @@ class TCC:
     def Logoff(self):
         self.Logged_In=False
 
-        r=requests.get(self.TCC_API + "logoff",cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "logoff",cookies=self.login_cookies,timeout=300)
         if r.status_code != 200:
             self.logger.error("Could not logout of  TCC")
         else:
@@ -72,7 +72,7 @@ class TCC:
         if not self.Logged_In:
             raise ("Not Logged into TCC in Get File Spaces")
 
-        r=requests.get(self.TCC_API + "GetFileSpaces?filter="+filter,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetFileSpaces?filter="+filter,cookies=self.login_cookies,timeout=300)
         if r.status_code != 200:
             self.logger.error("Could not get filespaces from TCC")
             return (None)
@@ -85,7 +85,7 @@ class TCC:
         if not self.Logged_In:
             raise ("Not Logged into TCC in Get File Spaces")
 
-        r=requests.get(self.TCC_API + "GetFileSpaceStatistics?filespaceid="+filespaceid,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetFileSpaceStatistics?filespaceid="+filespaceid,cookies=self.login_cookies,timeout=300)
         self.logger.debug(self.TCC_API + "GetFileSpaceStatistics?filespaceid="+filespaceid)
         if r.status_code != 200:
             self.logger.error("Could not get FileSpaceStatistics from TCC")
@@ -98,7 +98,7 @@ class TCC:
         if not self.Logged_In:
             raise ("Not Logged into TCC in Get Organization")
 
-        r=requests.get(self.TCC_API + "GetOrganization?orgid="+orgid,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetOrganization?orgid="+orgid,cookies=self.login_cookies,timeout=300)
         if r.status_code != 200:
             self.logger.error("Could not GetOrganization from TCC")
             return (None)
@@ -110,7 +110,7 @@ class TCC:
         if not self.Logged_In:
             raise ("Not Logged into TCC in GetOrganizationTags")
 
-        r=requests.get(self.TCC_API + "GetOrganizationTags?orgid="+orgid,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetOrganizationTags?orgid="+orgid,cookies=self.login_cookies,timeout=300)
         if r.status_code != 200:
             self.logger.error("Could not GetOrganizationTags from TCC")
             return (None)
@@ -137,18 +137,83 @@ class TCC:
 
         return(filespace_ID)
 
-    def Dir(self,filespace_ID,filemasklist=""):
 
-        if filemasklist != "":
-           r=requests.get(self.TCC_API + "Dir?recursive=true&path=/&filterfolders=true&filespaceId="+filespace_ID+"&filemasklist="+filemasklist,cookies=self.login_cookies)
-        else:
-           r=requests.get(self.TCC_API + "Dir?recursive=true&path=/&filterfolders=true&filespaceId="+filespace_ID,cookies=self.login_cookies)
 
-        if r.status_code != 200:
-           self.logger.error("Failed to get Dir: Error Code ("+str(r.status_code)+")")
-           return(None)
+    def Find_PL_ID(self,filespaces):
+        if filespaces == None:
+            return (None)
+        filespace_ID=""
+        PL="Project Library".upper()
+#        pprint(filespaces)
+        for filespace in filespaces["filespaces"]:
+        #   print filespace["title"]
+          if filespace["title"].upper() == PL:
+             filespace_ID=filespace["fileSpaceId"]
+             break
+
+        if filespace_ID=="":
+          self.logger.error("Could not find PL file space")
+          return(None)
         else:
-           return(r.json())
+          self.logger.debug("Got PL filespace: " + filespace_ID)
+
+        return(filespace_ID)
+
+
+    def FileSpace_ID_generator(self,fileSpaces,searchItem):
+        filespace_ID=""
+        searchItem=searchItem.upper()
+        for filespace in fileSpaces["filespaces"]:
+#          print (filespace["title"])
+          if filespace["title"].upper() == searchItem:
+             filespaceID=filespace["fileSpaceId"]
+             orgShortname=filespace["orgShortname"]
+             yield (filespaceID,orgShortname)
+
+
+    def Find_PL_IDs(self,filespaces):
+        if filespaces == None:
+            return (None)
+        filespace_ID=""
+        PL="Project Library".upper()
+#        pprint(filespaces)
+        for filespace in filespaces["filespaces"]:
+        #   print filespace["title"]
+          if filespace["title"].upper() == PL:
+             filespace_ID=filespace["fileSpaceId"]
+             break
+
+        if filespace_ID=="":
+          self.logger.error("Could not find PL file space")
+          return(None)
+        else:
+          self.logger.debug("Got PL filespace: " + filespace_ID)
+
+        return(filespace_ID)
+
+
+
+
+    def Dir(self,filespace_ID,filemasklist="",recursive=True,path=""):
+
+#        print("In Dir")
+        try:
+            if filemasklist != "":
+    #           print(self.TCC_API + "Dir?recursive=" + str(recursive) + "&path=/" + path + "&filterfolders=true&filespaceId="+filespace_ID+"&filemasklist="+filemasklist)
+               r=requests.get(self.TCC_API + "Dir?recursive=" + str(recursive) + "&path=/" + path + "&filterfolders=true&filespaceId="+filespace_ID+"&filemasklist="+filemasklist,cookies=self.login_cookies,timeout=300)
+            else:
+    #           print(self.TCC_API + "Dir?recursive=" + str(recursive) + "&path=/" + path + "&filterfolders=true&filespaceId="+filespace_ID)
+               r=requests.get(self.TCC_API + "Dir?recursive=" + str(recursive) + "&path=/" + path + "&filterfolders=true&filespaceId="+filespace_ID,cookies=self.login_cookies,timeout=300)
+
+            if r.status_code != 200:
+               self.logger.error("Failed to get Dir: " + path + " Error Code ("+str(r.status_code)+")")
+    #           pprint(r.json())
+               return(None)
+            else:
+               return(r.json())
+        except:
+               return(None)
+
 
     def Download(self,filespace_ID,TCC_File,Local_FileName):
         chunk_size=1024*60
@@ -158,13 +223,20 @@ class TCC:
 
         self.logger.debug("About to download: " + TCC_File + " to " + Local_FileName)
 
-        r=requests.get(self.TCC_API +"files?filespaceid=" + filespace_ID + '&path=%2F'+urllib.quote(TCC_File,'()'),stream=True,cookies=self.login_cookies)
+        try:
+            r=requests.get(self.TCC_API +"files?filespaceid=" + filespace_ID + '&path=/'+urllib.parse.quote(TCC_File,'()'),stream=True,cookies=self.login_cookies,timeout=300)
+        except:
+            return(False)
+
         OK=None
         if r.status_code == 200:
            OK=True
            with open(Local_FileName, 'wb') as fd:
-               for chunk in r.iter_content(chunk_size):
-                   fd.write(chunk)
+               try:
+                   for chunk in r.iter_content(chunk_size):
+                       fd.write(chunk)
+               except:
+                   pass
 
            self.logger.debug("Downloaded: " + TCC_File + " to " + Local_FileName)
         else:
@@ -180,27 +252,27 @@ class TCC:
 
     def GetDevices(self,orgid=""):
         if orgid == "":
-           r=requests.get(self.TCC_API + "GetDevices",cookies=self.login_cookies)
+           r=requests.get(self.TCC_API + "GetDevices",cookies=self.login_cookies,timeout=300)
         else:
-           r=requests.get(self.TCC_API + "GetDevices?orgid="+orgid,cookies=self.login_cookies)
+           r=requests.get(self.TCC_API + "GetDevices?orgid="+orgid,cookies=self.login_cookies,timeout=300)
         return(r.json())
 
     def GetLoginAccounts(self,orgid=""):
         if orgid == "":
-           r=requests.get(self.TCC_API + "GetLoginAccounts",cookies=self.login_cookies)
+           r=requests.get(self.TCC_API + "GetLoginAccounts",cookies=self.login_cookies,timeout=300)
         else:
-           r=requests.get(self.TCC_API + "GetLoginAccounts?orgid="+orgid,cookies=self.login_cookies)
+           r=requests.get(self.TCC_API + "GetLoginAccounts?orgid="+orgid,cookies=self.login_cookies,timeout=300)
         return(r.json())
 
     def GetLoginAccount(self):
-        r=requests.get(self.TCC_API + "GetLoginAccount?loginaccountid="+self.accountid,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetLoginAccount?loginaccountid="+self.accountid,cookies=self.login_cookies,timeout=300)
         return(r.json())
 
 
 
     def GetOrganizationDashboard(self,orgtypefilter="",licensefilter="",orgtagfilter="",organizationProfiles="",membershipDetails="",deviceDetails="",groupDetails=""):
         self.logger.debug("About to Get Org Dashboard: " +self.TCC_API + "GetOrganizationDashboard?membershipDetails="+membershipDetails+"&deviceDetails="+deviceDetails+"&organizationProfiles="+organizationProfiles)
-        r=requests.get(self.TCC_API + "GetOrganizationDashboard?membershipDetails="+membershipDetails+"&deviceDetails="+deviceDetails+"&organizationProfiles="+organizationProfiles,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetOrganizationDashboard?membershipDetails="+membershipDetails+"&deviceDetails="+deviceDetails+"&organizationProfiles="+organizationProfiles,cookies=self.login_cookies,timeout=300)
         self.ORG_DETAILS=r.json()
         return(self.ORG_DETAILS)
 
@@ -230,7 +302,7 @@ class TCC:
             raise ("Not Logged into TCC in Get Mountpoints")
 
         self.logger.debug("About to Get Org Mountpoints: " +self.TCC_API + "GetMountPoints?orgid="+orgid+"&MountPointTypeFilter="+MountPointTypeFilter+"&WantAllPublic="+wantallpublic+"&getinactive="+getinactive)
-        r=requests.get(self.TCC_API + "GetMountPoints?orgid="+orgid+"&MountPointTypeFilter="+MountPointTypeFilter+"&WantAllPublic="+wantallpublic+"&getinactive="+getinactive,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetMountPoints?orgid="+orgid+"&MountPointTypeFilter="+MountPointTypeFilter+"&WantAllPublic="+wantallpublic+"&getinactive="+getinactive,cookies=self.login_cookies,timeout=300)
         return(r.json())
 
     def GetMountPointConnectionList(self,mountpointid):
@@ -238,7 +310,7 @@ class TCC:
             raise ("Not Logged into TCC in GetMountPointConnectionList")
 
         self.logger.debug("About to GetMountPointConnectionList: " +self.TCC_API + "GetMountPointConnectionList?mountpointid="+mountpointid)
-        r=requests.get(self.TCC_API + "GetMountPointConnectionList?mountpointid="+mountpointid,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetMountPointConnectionList?mountpointid="+mountpointid,cookies=self.login_cookies,timeout=300)
         return(r.json())
 
     def GetMountPointDetails(self,mountpointid):
@@ -246,7 +318,7 @@ class TCC:
             raise ("Not Logged into TCC in GetMountPointDetails")
 
         self.logger.debug("About to GetMountPointDetails: " +self.TCC_API + "GetMountPointDetails?mountpointid="+mountpointid)
-        r=requests.get(self.TCC_API + "GetMountPointDetails?mountpointid="+mountpointid,cookies=self.login_cookies)
+        r=requests.get(self.TCC_API + "GetMountPointDetails?mountpointid="+mountpointid,cookies=self.login_cookies,timeout=300)
         return(r.json())
 
 
