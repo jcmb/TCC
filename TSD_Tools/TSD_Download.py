7c48565a962651ea3058b4274250985ee3e5ae69
jcmb <jcmb@users.noreply.github.com>
2023-06-19 16:38:08 +0300

    Add support for Project Library downloading.

--- TSD_Tools/TSD_Download.py
+++ TSD_Tools/TSD_Download.py
@@ -1,8 +1,10 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
+
+import sys
+sys.path.append("/Users/gkirk/Documents/GitHub/TCC")
 
 import requests
 from pprint import pprint
-import sys
 import logging
 import json
 import os
@@ -28,6 +30,17 @@ def parse_args():
 
    argp.add_argument('-t', '--type', metavar='EXTENSION', default="",
                    help='file types to be downloaded')
+
+   group = argp.add_mutually_exclusive_group()
+   group.add_argument('-a', '--ALL', action='store_true', default=False,
+                   help='If to download from all GCS900 and Earthworks filespaces')
+
+   group.add_argument('-g', '--TSD', action='store_true', default=False,
+                   help='If to download from all Trimble Synchronizer Data (GCS900 & SCS900/SiteWorks) filespaces')
+
+   group.add_argument('-l', '--PL', action='store_true', default=False,
+                   help='If to download from all Earthworks project library filespaces')
+
 
    argp.add_argument('--noarchived', action='store_true',
                    help='Ignore files in the Production-Data (Archived) folder')
@@ -60,39 +73,48 @@ def process_args(args):
    PASSWD=args.password
    TYPES=args.type
    NO_ARCHIVE=args.noarchived
-   
+   ALL=args.ALL
+   TSD=args.TSD
+   PL=args.PL
+
    if args.Tell:
       sys.stderr.write("User: {} Org: {}\n".format(USER,ORG))
       sys.stderr.write("Types: {}\n".format(TYPES))
       sys.stderr.write("Skip Archived: {}\n".format(NO_ARCHIVE))
+      sys.stderr.write("All Orgs (GCS900 and EW): {}\n".format(ALL))
+      sys.stderr.write("All Orgs (TSD Only): {}\n".format(TSD))
+      sys.stderr.write("All Orgs (PL Only): {}\n".format(PL))
       sys.stderr.write("Verbose: {}\n".format(Verbose))
       sys.stderr.write("\n")
-   return (USER,ORG,PASSWD,TYPES,NO_ARCHIVE,Verbose)
+   return (USER,ORG,PASSWD,TYPES,NO_ARCHIVE,ALL,TSD,PL,Verbose)
 
 
 
-def process_directory(dir,filespace_ID,tcc,NO_ARCHIVE):
+def process_directory(dir,filespace_ID,tcc,NO_ARCHIVE,TYPES,path=""):
+#   print("Path: " + path)
    for entry in dir:
 #      pprint (entry)
       if entry["isFolder"]:
          try:
-#            print "Dir: " + entry["entryName"]
-            os.mkdir(entry["entryName"])
+            print ("Dir: " + path + entry["entryName"])
+            os.mkdir("./"+path+entry["entryName"])
          except:
             logging.debug("Failed to make Dir: " + entry["entryName"])
             pass #Assume that the dir is already there
-         process_directory(entry["entries"],filespace_ID,tcc,NO_ARCHIVE)
+         data=tcc.Dir(filespace_ID,TYPES,False,path+entry["entryName"])
+         if data != None:
+             process_directory(data["entries"],filespace_ID,tcc,NO_ARCHIVE,TYPES,path+entry["entryName"]+'/')
       else:
          if (not NO_ARCHIVE) or (entry["entryName"].find("Production-Data (Archived)")==-1):
-            if os.path.isfile(entry["entryName"]) and (os.path.getsize(entry["entryName"]) == int(entry["size"])):
-              print 'Skipping (Downloaded already): '+ entry["entryName"] 
-            else:             
-              if tcc.Download(filespace_ID,entry["entryName"],entry["entryName"]):
-                 print 'Downloaded: '+ entry["entryName"]
+            if os.path.isfile(path+entry["entryName"]) and (os.path.getsize(path+entry["entryName"]) == int(entry["size"])):
+              print('Skipping (Downloaded already): '+ path+ entry["entryName"])
+            else:
+              if tcc.Download(filespace_ID,path+entry["entryName"],"./"+path+entry["entryName"]):
+                 print('Downloaded: '+ path+entry["entryName"])
               else:
-                 print 'Failed to download: '+ entry["entryName"]
+                 print('Failed to download: '+ entry["entryName"])
          else:
-            print 'Skipping (Archived): '+ entry["entryName"]
+            print('Skipping (Archived): '+ entry["entryName"])
 
 
 # The dir json is a list of entries, if it is a folder then it has a a list of entrys which might be more directories, welcome to recursion
@@ -100,21 +122,40 @@ def process_directory(dir,filespace_ID,tcc,NO_ARCHIVE):
 
 def main():
     args=parse_args()
-    (USER,ORG,PASSWD,TYPES,NO_ARCHIVE,Verbose)=process_args(args)
+    (USER,ORG,PASSWD,TYPES,NO_ARCHIVE,ALL,TSD,PL,Verbose)=process_args(args)
     tcc=TCC.TCC(USER,ORG,PASSWD,Verbose)
     if tcc.Login("JCMBsoft_TSD_Download"):
 
-        filespaces=tcc.GetFileSpaces()
+        if ALL or PL or TSD:
+            filespaces=tcc.GetFileSpaces(filter="all")
+        else:
+            filespaces=tcc.GetFileSpaces()
 
-        TSD_ID=tcc.Find_TSD_ID(filespaces)
+        if not TSD:
+            for (filespace,orgShortname) in tcc.FileSpace_ID_generator(filespaces,"Project Library"):
+                print ("Org: " + orgShortname, end = '')
+                data=tcc.Dir(filespace,TYPES,False)
 
-        if TSD_ID == None:
-            raise ("Could not find TSD")
+                if data == None:
+                    print(". Dir Failed for " + orgShortname + ":Project Library")
+                else:
+                    print (". Dir Processed")
+                    os.makedirs(orgShortname + os.sep  + "Project Library",exist_ok=True)
+                    os.chdir(orgShortname + os.sep  + "Project Library")
+                    process_directory(data["entries"],filespace,tcc,NO_ARCHIVE,TYPES)
+                    os.chdir(".." + os.sep  + ".." )
 
-        data=tcc.Dir(TSD_ID,TYPES)
-
-# The dir json is a list of entries, if it is a folder then it has a a list of entrys which might be more directories, welcome to recursion
-        process_directory(data["entries"],TSD_ID,tcc,NO_ARCHIVE)
+        if not PL:
+            for (filespace,orgShortname) in tcc.FileSpace_ID_generator(filespaces,"Trimble Synchronizer Data"):
+                print ("Org: " + orgShortname)
+                data=tcc.Dir(filespace,TYPES,False)
+                if data == None:
+                    print("Dir Failed for " + orgShortname + ":Project Library")
+                else:
+                    os.makedirs(orgShortname + os.sep  + "Trimble Synchronizer Data",exist_ok=True)
+                    os.chdir(orgShortname + os.sep  + "Trimble Synchronizer Data")
+                    process_directory(data["entries"],filespace,tcc,NO_ARCHIVE,TYPES)
+                    os.chdir(".." + os.sep  + ".." )
 
         tcc.Logoff()
 
